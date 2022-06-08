8f48c5c52f35eed9e43269ee57eec4a3f964fbd8
Deiferson Moura <deifersondasilvamoura@gmail.com>
2022-06-08 14:56:13 -0300

    init. edit initcompoment and create interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+interface users {
+  id: number;
+  name: string;
+  email:string;
+}
+
+const USERS: users[] = [
+  {
+    id: 1,
+    name: 'Ana',
+    email: 'ana@gmail.com'
+  },  
+  {
+    id: 1,
+    name: 'Ana',
+    email: 'ana@gmail.com'
+  },  
+  {
+    id: 1,
+    name: 'Ana',
+    email: 'ana@gmail.com'
+  },  
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pso';
+  users = USERS;
 }
